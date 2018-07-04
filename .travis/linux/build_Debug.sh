eb279d06a25e3d4aaaa1fd1c44b26fa6ddae134d
Juan Manuel Carmona Loaiza <juanmcloaiza@gmail.com>
2018-07-04 18:48:08 +0200

    Testing automatic AppImage build by Travis

--- .travis/linux/build_Debug.sh
+++ .travis/linux/build_Debug.sh
@@ -4,5 +4,14 @@ export PATH=$QTDIR/bin:$PATH
 git submodule update --init --recursive
 mkdir build
 cd build
-cmake -D CMAKE_BUILD_TYPE=$BUILD_TYPE -D WITH_TESTS=1 ..
-make -j9
+cp -r ../AppDir ./
+cmake -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DWITH_TESTS=1 -DCMAKE_INSTALL_PREFIX=/usr..
+make -j$(nproc)
+make DESTDIR=AppDir -j$(nproc) install ; find AppDir/
+
+wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
+chmod a+x linuxdeployqt-continuous-x86_64.AppImage
+unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
+export VERSION=$(git rev-parse --short HEAD) # linuxdeployqt uses this for naming the file
+./linuxdeployqt-continuous-x86_64.AppImage AppDir/usr/share/applications/*.desktop -bundle-non-qt-libs
+./linuxdeployqt-continuous-x86_64.AppImage AppDir/usr/share/applications/*.desktop -appimage
