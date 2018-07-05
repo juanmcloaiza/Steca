fd5554bc63589ea826e478cdc1bec86728da60af
Juan Manuel Carmona Loaiza <juanmcloaiza@gmail.com>
2018-07-05 10:58:56 +0200

    Putting things as they were -- this should not brake travis now

--- .travis/linux/build_Debug.sh
+++ .travis/linux/build_Debug.sh
@@ -4,15 +4,16 @@ export PATH=$QTDIR/bin:$PATH
 git submodule update --init --recursive
 mkdir build
 cd build
-cp -r ../AppDir ./
-cmake -D CMAKE_BUILD_TYPE=$BUILD_TYPE -D WITH_TESTS=1 .. #-D CMAKE_INSTALL_PREFIX=/usr ..
+cmake -D CMAKE_BUILD_TYPE=$BUILD_TYPE -D WITH_TESTS=1 ..
 make -j$(nproc)
 
-cp ./main/Steca ./AppDir/usr/bin
 
-wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
-chmod a+x linuxdeployqt-continuous-x86_64.AppImage
-unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
-export VERSION=$(git rev-parse --short HEAD) # linuxdeployqt uses this for naming the file
-./linuxdeployqt-continuous-x86_64.AppImage AppDir/usr/share/applications/*.desktop -bundle-non-qt-libs
-./linuxdeployqt-continuous-x86_64.AppImage AppDir/usr/share/applications/*.desktop -appimage
+#cp -r ../AppDir ./
+#cp ./main/Steca ./AppDir/usr/bin
+
+#wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
+#chmod a+x linuxdeployqt-continuous-x86_64.AppImage
+#unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
+#export VERSION=$(git rev-parse --short HEAD) # linuxdeployqt uses this for naming the file
+#./linuxdeployqt-continuous-x86_64.AppImage AppDir/usr/share/applications/*.desktop -bundle-non-qt-libs
+#./linuxdeployqt-continuous-x86_64.AppImage AppDir/usr/share/applications/*.desktop -appimage
