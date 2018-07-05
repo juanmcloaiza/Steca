e5fc692844e468889cfe1baf6829f041a3def06b
Juan Manuel Carmona Loaiza <juanmcloaiza@gmail.com>
2018-07-05 11:11:29 +0200

    Testing automatic AppImage build by Travis -- 5th attempt

--- .travis/linux/build_Release.sh
+++ .travis/linux/build_Release.sh
@@ -6,3 +6,23 @@ mkdir build
 cd build
 cmake -D CMAKE_BUILD_TYPE=$BUILD_TYPE -D WITH_TESTS=1 ..
 make -j9
+
+
+echo "Copying AppDir..."
+cp -r ../AppDir ./
+
+echo "Copying Steca binary into AppDir..."
+cp ./main/Steca ./AppDir/usr/bin
+
+
+echo "wgetting linuxdeployqt..."
+wget -c -nv "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
+
+echo "chmod linuxdeployqt and setting env vars..."
+chmod a+x linuxdeployqt-continuous-x86_64.AppImage
+unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
+export VERSION=$(git rev-parse --short HEAD) # linuxdeployqt uses this for naming the file
+
+echo "Running linuxdeployqt..."
+./linuxdeployqt-continuous-x86_64.AppImage AppDir/usr/share/applications/*.desktop -bundle-non-qt-libs
+./linuxdeployqt-continuous-x86_64.AppImage AppDir/usr/share/applications/*.desktop -appimage
